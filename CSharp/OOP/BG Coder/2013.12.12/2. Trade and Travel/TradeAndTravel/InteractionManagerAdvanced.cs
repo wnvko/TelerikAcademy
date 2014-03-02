a9b1f481dbcf419acd80dae132e1f89d3eaa4962
wnvko <milko.venkov@gmail.com>
2014-03-02 22:42:34 +0200

    OOP Bg Coder 2013.12.12

---
+++ CSharp/OOP/BG Coder/2013.12.12/2. Trade and Travel/TradeAndTravel/InteractionManagerAdvanced.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeAndTravel
+{
+    class InteractionManagerAdvanced : InteractionManager
+    {
+        public InteractionManagerAdvanced()
+            : base()
+        { }
+
+        protected override Item CreateItem(string itemTypeString, string itemNameString, Location itemLocation, Item item)
+        {
+
+            switch (itemTypeString)
+            {
+                case "armor":
+                    {
+                        return base.CreateItem(itemTypeString, itemNameString, itemLocation, item);
+                        break;
+                    }
+                case "weapon":
+                    {
+                        item = new Weapon(itemNameString, itemLocation);
+                        break;
+                    }
+                case "wood":
+                    {
+                        item = new Wood(itemNameString, itemLocation);
+                        break;
+                    }
+                case "iron":
+                    {
+                        item = new Iron(itemNameString, itemLocation);
+                        break;
+                    }
+                default:
+                    break;
+            }
+            return item;
+        }
+
+        protected override Location CreateLocation(string locationTypeString, string locationName)
+        {
+            Location location = null;
+            switch (locationTypeString)
+            {
+                case "town":
+                    {
+                        return base.CreateLocation(locationTypeString, locationName);
+                    }
+                case "mine":
+                    {
+                        location = new Mine(locationName);
+                        break;
+                    }
+                case "forest":
+                    {
+                        location = new Forest(locationName);
+                        break;
+                    }
+                default:
+                    break;
+            }
+            return location;
+        }
+
+        protected override void HandlePersonCommand(string[] commandWords, Person actor)
+        {
+            switch (commandWords[1])
+            {
+                case "drop":
+                case "pickup":
+                case "sell":
+                case "buy":
+                case "inventory":
+                case "money":
+                case "travel":
+                    {
+                        base.HandlePersonCommand(commandWords, actor);
+                        break;
+                    }
+                case "gather":
+                    {
+                        HandleGaterInteraction(commandWords, actor);
+                        break;
+                    }
+                case "craft":
+                    {
+                        HandleCraftCommand(commandWords, actor);
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        protected override Person CreatePerson(string personTypeString, string personNameString, Location personLocation)
+        {
+            Person person = null;
+            switch (personTypeString)
+            {
+                case "shopkeeper":
+                case "traveller":
+                    {
+                        return base.CreatePerson(personTypeString, personNameString, personLocation);
+                        break;
+                    }
+                case "merchant":
+                    {
+                        person = new Traveller(personNameString, personLocation);
+                        break;
+                    }
+                default:
+                    break;
+            }
+            return person;
+
+        }
+
+        private void HandleGaterInteraction(string[] commandWords, Person actor)
+        {
+            var inventory = actor.ListInventory();
+
+            if (actor.Location.LocationType == LocationType.Forest)
+            {
+                foreach (var item in inventory)
+                {
+                    if (ownerByItem[item] == actor && item.ItemType == ItemType.Weapon)
+                    {
+                        var woodenItem = new Wood(commandWords[2]);
+                        this.AddToPerson(actor, woodenItem);
+
+                        woodenItem.UpdateWithInteraction("gather");
+                    }
+                }
+            }
+
+            if (actor.Location.LocationType == LocationType.Mine)
+            {
+                foreach (var item in inventory)
+                {
+                    if (ownerByItem[item] == actor && item.ItemType == ItemType.Armor)
+                    {
+                        var ironItem = new Iron(commandWords[2]);
+                        this.AddToPerson(actor, ironItem);
+
+                        ironItem.UpdateWithInteraction("gather");
+                    }
+                }
+            }
+
+        }
+
+        private void HandleCraftCommand(string[] commandWords, Person actor)
+        {
+            var inventory = actor.ListInventory();
+            bool hasIron = false;
+            bool hasWood = false;
+
+            foreach (var item in inventory)
+            {
+                if (ownerByItem[item] == actor)
+                {
+                    if (item.ItemType == ItemType.Iron)
+                    {
+                        hasIron = true;
+                    }
+
+                    if (item.ItemType == ItemType.Wood)
+                    {
+                        hasWood = true;
+                    }
+                }
+            }
+
+            
+
+            switch (commandWords[2])
+            {
+                case "armor":
+                    {
+                        if (commandWords[2] == "armor" && hasIron)
+                        {
+                            var armorItem = new Armor(commandWords[3]);
+                            this.AddToPerson(actor, armorItem);
+
+                            armorItem.UpdateWithInteraction("crafted");
+                        }
+
+                        break;
+                    }
+                case "weapon":
+                    {
+                        if (hasWood && hasIron)
+                        {
+                            var weaponItem = new Weapon(commandWords[3]);
+                            this.AddToPerson(actor, weaponItem);
+
+                            weaponItem.UpdateWithInteraction("crafted");
+                        }
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ CSharp/OOP/BG Coder/2013.12.12/2. Trade and Travel/TradeAndTravel/Wood.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TradeAndTravel
+{
+    class Wood : Item
+    {
+        const int GeneralWoodValue = 2;
+        public int Value { get; set; }
+
+        public Wood(string name, Location location = null)
+            : base(name, Wood.GeneralWoodValue, ItemType.Wood, location)
+        {
+            this.Value = GeneralWoodValue;
+        }
+
+        public override void UpdateWithInteraction(string interaction)
+        {
+            switch (interaction)
+            {
+                case "drop":
+                    {
+                        if (this.Value > 0)
+                        {
+                            this.Value--;
+                        }
+
+                        break;
+                    }
+                default:
+                    {
+                        base.UpdateWithInteraction(interaction);
+                        break;
+                    }
+            }
+        }
+
+        static List<ItemType> GetComposingItems()
+        {
+            return new List<ItemType>() { ItemType.Wood };
+        }
+    }
+}
