1608db6fe475e25399a104dce7bed4b095079324
wnvko <milko.venkov@gmail.com>
2014-05-04 01:12:28 +0300

    DSA

--- CSharp/DSA/II. Linear Data Structures/SumAndAverage/SumAndAverage.cs
+++ CSharp/DSA/II. Linear Data Structures/SumAndAverage/SumAndAverage.cs
@@ -1,32 +1,29 @@
-﻿namespace SumAndAverage
+﻿using System;
+using System.Collections.Generic;
+
+public class SumAndAverage
 {
-    using System;
-    using System.Collections.Generic;
-
-    public class SumAndAverage
+    public static void Main()
     {
-        public static void Main()
+        Console.WriteLine("Please enter some numbers");
+        Console.WriteLine("Enter empty line to finish");
+
+        List<int> inputData = new List<int>();
+        int sum = 0;
+
+        while (true)
         {
-            Console.WriteLine("Please enter some numbers");
-            Console.WriteLine("Enter empty line to finish");
-
-            List<int> inputData = new List<int>();
-            int sum = 0;
-
-            while (true)
+            string currentInput = Console.ReadLine();
+            if (currentInput == string.Empty)
             {
-                string currentInput = Console.ReadLine();
-                if (currentInput == string.Empty)
-                {
-                    break;
-                }
-
-                inputData.Add(int.Parse(currentInput));
-                sum += inputData[(inputData.Count - 1)];
+                break;
             }
 
-            Console.WriteLine("The sum of elements is: {0}", sum);
-            Console.WriteLine("The average of elements is: {0}", sum / inputData.Count);
+            inputData.Add(int.Parse(currentInput));
+            sum += inputData[(inputData.Count - 1)];
         }
+
+        Console.WriteLine("The sum of elements is: {0}", sum);
+        Console.WriteLine("The average of elements is: {0}", sum / inputData.Count);
     }
 }
