b403aa8fe1557cc5b0d91d02b860dc0d3b1f0fe8
wnvko <milko.venkov@gmail.com>
2014-05-05 19:56:05 +0300

    DSA tree repair

---
+++ CSharp/DSA/III. Trees and Traversals/AllExeInWindows/AllExeInWindows.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class AllExeInWindows
+{
+    public const string WindowsPath = @"D:\TelerikAcademy";
+    public const string FilePatern = @"*.cs";
+    public static int filesCount = 0;
+
+    public static void Main()
+    {
+        try
+        {
+            if (File.Exists(WindowsPath))
+            {
+                ProcessFile(WindowsPath);
+            }
+            else if (Directory.Exists(WindowsPath))
+            {
+                ProcessDirectory(WindowsPath);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a lalid path", WindowsPath);
+            }
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine("Unauthorized access! Directory/Files cannot be traversed");
+        }
+        
+
+        Console.WriteLine();
+        Console.WriteLine("In total {0} files of type {1}", filesCount, FilePatern);
+    }
+
+    private static void ProcessDirectory(string windowsPath)
+    {
+        string[] files = Directory.GetFiles(windowsPath, FilePatern);
+        foreach (var file in files)
+        {
+            ProcessFile(file);
+        }
+
+        string[] subDirs = Directory.GetDirectories(windowsPath);
+        foreach (var subDir in subDirs)
+        {
+            ProcessDirectory(subDir);
+        }
+    }
+
+    private static void ProcessFile(string windowsPath)
+    {
+        Console.WriteLine("Files {0}", windowsPath);
+        filesCount++;
+    }
+}
