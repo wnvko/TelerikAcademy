f436cc1cd859cfdc77e64c5522bea39783d71402
Milko Venkov <milko.venkov@gmail.com>
2014-05-11 03:43:21 +0300

    JS Fundamentals Loops

---
+++ WebAndUI/JS Fundamentals/06. Loops/scripts/02. NumbersNotDivisibleTo3And7.js
@@ -0,0 +1,34 @@
+﻿var upperLimit;
+var textBoxLabel = 'Enter number N: ';
+
+function setTextBoxOnFocus() {
+    var textBox = document.getElementById('tb-number');
+    textBox.focus();
+    textBox.select();
+}
+setTextBoxOnFocus();
+
+function takeValue() {
+    var labelTitle = document.getElementById('label');
+    upperLimit = document.getElementById('tb-number').value;
+    labelTitle.innerHTML = textBoxLabel;
+    switchNumbers();
+}
+
+function takeValueOnEnter(e) {
+    if (e.keyCode == 13) {
+        takeValue();
+    }
+}
+
+function switchNumbers() {
+    upperLimit = parseInt(upperLimit);
+
+    for (var i = 1; i <= upperLimit; i++) {
+        if (i % 3 === 0 && i % 7 === 0) {
+            continue;
+        }
+
+        jsConsole.writeLine(i);
+    }
+}
---
+++ WebAndUI/JS Fundamentals/06. Loops/scripts/04. Properties.js
@@ -0,0 +1,49 @@
+﻿var documentFirst;
+var documentLast;
+var windowFirst;
+var windowLast;
+var navigatorFirst;
+var navigatorLast;
+
+var documentProperties;
+var windowProperties;
+var navigatorProperties;
+
+documentProperties = Document;
+documentFirst = findFirstAndLastInColection(documentProperties).first;
+documentLast = findFirstAndLastInColection(documentProperties).last;
+
+windowProperties = Window;
+windowFirst = findFirstAndLastInColection(windowProperties).first;
+windowLast = findFirstAndLastInColection(windowProperties).last;
+
+navigatorProperties = Navigator;
+navigatorFirst = findFirstAndLastInColection(navigatorProperties).first;
+navigatorLast = findFirstAndLastInColection(navigatorProperties).last;
+
+jsConsole.writeLine(documentFirst);
+jsConsole.writeLine(documentLast);
+jsConsole.writeLine(windowFirst);
+jsConsole.writeLine(windowLast);
+jsConsole.writeLine(navigatorFirst);
+jsConsole.writeLine(navigatorLast);
+
+
+function findFirstAndLastInColection(properties) {
+    var borderEntities = {
+        first: 'Z',
+        last: 'A',
+    };
+
+    for (var property in properties) {
+        if (borderEntities.first > property) {
+            borderEntities.first = property;
+        }
+
+        if (borderEntities.last < property) {
+            borderEntities.last = property;
+        }
+    }
+
+    return borderEntities;
+}
