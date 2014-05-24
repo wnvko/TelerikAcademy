caa5372230bfe652c6d5e0610363d870e4c897b5
Milko Venkov <milko.venkov@gmail.com>
2014-05-24 06:25:48 +0300

    HQC Variables ok

---
+++ CSharp/HQC/05. Using Variables/DataEvaluator/Printer.cs
@@ -0,0 +1,46 @@
+﻿namespace DataEvaluator
+{
+    using System;
+
+    public class Printer
+    {
+        public void PrintStatistics(double[] input, int count)
+        {
+            double inputMaxValue = Double.MinValue;
+            for (int i = 0; i < count; i++)
+            {
+                if (input[i] > inputMaxValue)
+                {
+                    inputMaxValue = input[i];
+                }
+            }
+
+            Print(inputMaxValue);
+            
+            double inputMinValue = Double.MaxValue;
+            for (int i = 0; i < count; i++)
+            {
+                if (input[i] < inputMinValue)
+                {
+                    inputMinValue = input[i];
+                }
+            }
+
+            Print(inputMinValue);
+
+            double inputSum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                inputSum += input[i];
+            }
+
+            double inputAverage = inputSum / count;
+            Print(inputAverage);
+        }
+
+        public static void Print(double inputToPrint)
+        {
+            // Do something...
+        }
+    }
+}
