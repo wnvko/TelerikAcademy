24fb520580bacf7ac2153a723075c72c0b0154c7
Milko Venkov <milko.venkov@gmail.com>
2014-06-01 13:25:19 +0300

    JS UI DOM 1st start

---
+++ WebAndUI/JS UI and DOM/01. Document Object Model/scripts/01. DIVs.js
@@ -0,0 +1,24 @@
+﻿console.log('Live DIVs:');
+getLiveDivs();
+console.log('Static DIVs:');
+getStaticDivs();
+
+function getLiveDivs() {
+    var wrapperDivLive = document.getElementsByTagName('div');
+    var innerDivLive = [];
+
+    for (var i = 0, length = wrapperDivLive.length; i < length; i++) {
+        innerDivLive.push(wrapperDivLive[i].getElementsByTagName('div'));
+    }
+
+    for (var j = 0, length = innerDivLive.length; j < length; j++) {
+        console.log(innerDivLive[j]);
+    }
+}
+
+function getStaticDivs() {
+    var divStatic = document.querySelectorAll('div > div');
+    for (var i = 0, length = divStatic.length; i < length; i++) {
+        console.log(divStatic);
+    }
+}
