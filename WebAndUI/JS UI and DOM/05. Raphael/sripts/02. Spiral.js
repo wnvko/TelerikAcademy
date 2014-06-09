a8cc517f026324b1768c284ab53034eb3a687e88
wnvko <milko.venkov@gmail.com>
2014-06-09 23:36:59 +0300

    JS UI&DOM Raphael ok

---
+++ WebAndUI/JS UI and DOM/05. Raphael/sripts/02. Spiral.js
@@ -0,0 +1,39 @@
+﻿var paper = Raphael(250, 10, 500, 500);
+
+function drawSpyral() {
+    paper.clear();
+
+    var radius = 0,
+        angle = 0,
+        startX = 0,
+        startY = 0,
+        endX = paper.width / 2,
+        endY = paper.height / 2,
+        pathString = '',
+        spyral,
+        b;
+
+    b = document.getElementById('b').value;
+
+    if (b <= 0) {
+        b = 5;
+    }
+
+    while (true) {
+        startX = endX;
+        startY = endY;
+
+        radius = b * angle;
+        angle += 0.1;
+
+        endX = paper.width / 2 + radius * Math.cos(angle);
+        endY = paper.height / 2 + radius * Math.sin(angle);
+
+        pathString = 'M' + startX + ',' + startY + ' L' + endX + ',' + endY;
+        spyral = paper.path(pathString);
+
+        if (startX > paper.width && startY > paper.height) {
+            break;
+        }
+    }
+}
