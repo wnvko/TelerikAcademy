9f7b451d11b83cbb495621a8c561cdd141e04239
wnvko <milko.venkov@gmail.com>
2014-06-15 23:57:43 +0300

    HQC TDD only first task :(

---
+++ CSharp/HQC/12. Test Driven Development/Card.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Poker
+{
+    public class Card : ICard
+    {
+        private CardFace face;
+        private CardSuit suit;
+
+        public Card(CardFace face, CardSuit suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+        }
+
+        public CardFace Face
+        {
+            get
+            {
+                return this.face;
+            }
+
+            private set
+            {
+                this.face = value;
+            }
+        }
+        public CardSuit Suit
+        {
+            get
+            {
+                return this.suit;
+            }
+
+            private set
+            {
+                this.suit = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            string cardName = this.Face.ToString() + " " + this.Suit.ToString();
+            return cardName;
+        }
+    }
+}
---
+++ CSharp/HQC/12. Test Driven Development/Hand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker
+{
+    public class Hand : IHand
+    {
+        private IList<ICard> cards;
+
+        public Hand(IList<ICard> cards)
+        {
+            this.Cards = cards;
+        }
+
+        public IList<ICard> Cards
+        {
+            get
+            {
+                return this.cards;
+            }
+
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Cards list cannot be null");
+                }
+                if (value.Count <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Cards in hand cannot be zero or less.");
+                }
+
+                this.cards = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder cardsToString = new StringBuilder();
+            foreach (var card in this.Cards)
+            {
+                cardsToString.Append(card.ToString());
+                cardsToString.Append(string.Empty);
+            }
+
+            return cardsToString.ToString().TrimEnd();
+        }
+    }
+}
