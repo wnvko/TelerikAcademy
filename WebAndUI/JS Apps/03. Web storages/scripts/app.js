3a32edd1e99f7206dc5f062d898737d797e17600
wnvko <milko.venkov@gmail.com>
2014-07-26 20:45:33 +0300

    JS App 03.Sheep and Rams 90% ready

---
+++ WebAndUI/JS Apps/03. Web storages/scripts/app.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="lib/jquery-2.1.1.js" />
+/// <reference path="lib/require.js" />
+(function () {
+
+    requirejs.config({
+        paths: {
+            'jQuery': 'libs/jquery-2.1.1',
+            'Main': 'main',
+            'Random': 'randomNumberGetter',
+            'FindNumber': 'findNumber'
+        },
+        shim: {
+            'jQuery': {
+                exports: '$'
+            }
+        }
+    });
+
+    require(['Main', 'Random'], function (Main, rnd) {
+        Main();
+    });
+}());
---
+++ WebAndUI/JS Apps/03. Web storages/scripts/main.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="randomNumberGetter.js" />
+/// <reference path="lib/jquery-2.1.1.js" />
+define(['jQuery', 'Random', 'FindNumber'],
+    function ($, random, findNumber) {
+        'use strict';
+        var Main = function () {
+            var randomGetter = new random(),
+                numberToGuess = randomGetter.get(),
+                sheepAndRams;
+
+            console.log(numberToGuess);
+
+            var $buttonGetUserInput = $('.getUserInput').first();
+            $buttonGetUserInput.click(function () {
+                var finder = new findNumber(numberToGuess);
+                console.log(finder);
+                sheepAndRams =  finder.go();
+
+                if (sheepAndRams.rams === 4) {
+                    var playerName = prompt('You did it. Please enter your name!');
+                    console.log(playerName);
+                };
+            });
+        };
+
+        return Main
+    });
