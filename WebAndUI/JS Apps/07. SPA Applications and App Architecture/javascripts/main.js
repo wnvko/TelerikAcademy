afa4ad87d78c8cfab684f9d1195249b77156e487
wnvko <milko.venkov@gmail.com>
2014-07-31 16:22:25 +0300

    JS APPS - 07. SPA Applications and App Architecture ok

---
+++ WebAndUI/JS Apps/07. SPA Applications and App Architecture/javascripts/main.js
@@ -0,0 +1,30 @@
+﻿//POST
+//Body: { "user": "USER_NAME", 
+//        "text": "MESSAGE_TEXT"}
+
+//GET
+/// <reference path="jquery-2.1.1.js" />
+/// <reference path="sammy/sammy.js" />
+
+(function ($) {
+    var url = 'http://crowd-chat.herokuapp.com/posts';
+
+    var app = $.sammy('#main', function () {
+
+        this.get('#/', function (context) {
+            this.load(url)
+            .then(function (items) {
+                var $main = $('#main').append('<ul>');
+                items = $.parseJSON(items);
+                for (var i = 0, len = items.length; i < len; i++) {
+                    $main.append($('<li>').html(items[i].by + ' said: ' + items[i].text));
+                }
+            });
+        });
+    });
+
+    $(function () {
+        app.run('#/');
+    });
+
+})(jQuery);
