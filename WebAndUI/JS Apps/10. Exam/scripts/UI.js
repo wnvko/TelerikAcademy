1fbe95d4c6f498c5973a2607b7899c35aa1533b6
wnvko <milko.venkov@gmail.com>
2014-07-31 16:23:21 +0300

    JS Apps Exam ok

---
+++ WebAndUI/JS Apps/10. Exam/scripts/UI.js
@@ -0,0 +1,41 @@
+﻿/// <reference path="libs/handlebars-v1.3.0.js" />
+/// <reference path="libs/jquery-2.1.1.js" />
+/// <reference path="libs/require.js" />
+
+define(['jQuery', 'Handlebars'], function ($, handlebars) {
+    'use strict';
+    var UI = (function () {
+        function UI(params) {
+        };
+
+        UI.prototype.Post = function (posts) {
+            var $container = $('#post-window'),
+                $data = $('<dl>'),
+                userName,
+                postDate,
+                title,
+                body;
+
+            for (var i = 0, len = posts.length; i < len; i++) {
+                userName = posts[i].user.username;
+                postDate = posts[i].postDate;
+                title = posts[i].title;
+                body = posts[i].body;
+                $data.append($('<dt>').html(userName + ' said at ' + postDate));
+                $data.append($('<dd>').append($('<h5>').html(title)).append($('<p>').html(body)));
+            };
+            $container.html('');
+            $container.append($data);
+            //var a = $('#post-window').html('<p>' + JSON.stringify(posts) + '</p>');
+
+            //var postTemplate = hbars.Compile($('#postUI').html());
+            //var $('#post-window').html(postTemplate({
+            //    posts: posts
+            //}));
+        };
+
+        return UI;
+    }());
+
+    return UI;
+});
---
+++ WebAndUI/JS Apps/10. Exam/scripts/app.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    requirejs.config({
+        paths: {
+            jQuery: 'libs/jquery-2.1.1',
+            Q: 'libs/q',
+            Handlebars: 'libs/handlebars-v1.3.0',
+            Underscore: 'libs/underscore',
+            Main: 'main',
+            rest: 'restOpearations',
+            UI: 'UI',
+        },
+        shim: {
+            'jQuery': {
+                exports: '$'
+            },
+            'Q': {
+                exports: 'q'
+            },
+            'Handlebars': {
+                exports: 'handlebars'
+            },
+            'Underscore': {
+                exports: '_'
+            },
+        }
+    });
+
+    require(['Main'], function (Main) {
+        Main();
+    });
+}());
