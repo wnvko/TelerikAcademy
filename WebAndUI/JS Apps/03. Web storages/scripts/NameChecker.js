3abedb2f9a9e8591a84d67a6afcbf59e6558e421
wnvko <milko.venkov@gmail.com>
2014-08-08 23:09:44 +0300

    DB - 01. Introduction ok

---
+++ WebAndUI/JS Apps/03. Web storages/scripts/NameChecker.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="require.js" />
+/// <reference path="jquery-2.1.1.min.js" />
+'use strict';
+
+define(['Cookies'], function (cookies) {
+    var NameChecker = (function () {
+        function NameChecker(name) {
+            this.name = name || ' ';
+        }
+
+        var myCookie = new cookies();
+        myCookie.createCookies('milko', 'test', 1);
+        console.log(myCookie);
+
+        return NameChecker;
+    }());
+
+    return NameChecker;
+});
