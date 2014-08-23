ef89941ff2225a7a0495fde7f97bb953edc33788
wnvko <milko.venkov@gmail.com>
2014-08-23 14:50:12 +0300

    Entity framework performance - ok

---
+++ SoftwareTechnoligies/DataBases/09. Entity Framework Performance/EntityFrameworkPerformance/NPlusOneProblem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TelerikAcademy;
+
+namespace NPlusOneProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelerikAcademyEntities db = new TelerikAcademyEntities();
+
+            //foreach (var employee in db.Employees.Include("Address.Town").Include("Department"))
+            //{
+            //    Console.WriteLine("{0} from {1} is in {2} department", employee.FirstName, employee.Address.Town, employee.Department);
+            //}
+
+            foreach (var employee in db.Employees)
+            {
+                Console.WriteLine("{0} from {1} is in {2} department", employee.FirstName, employee.Address.Town, employee.Department);
+            }
+        }
+    }
+}
---
+++ SoftwareTechnoligies/DataBases/09. Entity Framework Performance/EntityFrameworkPerformance/ToListProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using TelerikAcademy;
+
+namespace ToListProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelerikAcademyEntities db = new TelerikAcademyEntities();
+            
+            //List<Town> badWay = db.Employees.ToList().Select(e => e.Address).ToList().Select(a => a.Town).ToList().Where(t => t.Name == "Sofia").ToList();
+            //foreach (var town in badWay)
+            //{
+            //    Console.WriteLine(town);
+            //}
+
+            IQueryable goodWay = db.Employees.Select(e => e.Address).Select(a => a.Town).Where(t => t.Name == "Sofia");
+            foreach (var town in goodWay)
+            {
+                Console.WriteLine(town);
+            }
+
+            Console.WriteLine("Please check screenshots in the archive!");
+        }
+    }
+}
