c411a094a4129800311b8866c8ef677a525290cc
wnvko <milko.venkov@gmail.com>
2014-09-01 22:29:25 +0300

    DSA - 06. Data Structures Efficiency - ok 2/3

---
+++ CSharp/DSA/06. Data Structures Efficiency/DataStructuresEfficiency/Students/Students.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Students
+{
+    private const string InputFile = @"..\..\students.txt";
+
+    static void Main(string[] args)
+    {
+        SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> students = new SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>();
+        List<string[]> inputData = new List<string[]>();
+
+        GetData(inputData);
+        FillData(students, inputData);
+        PrintData(students);
+    }
+
+    private static void GetData(List<string[]> inputData)
+    {
+        StreamReader reader = new StreamReader(InputFile);
+        using (reader)
+        {
+            while (true)
+            {
+                string line = reader.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+
+                inputData.Add(line.Split(new char[] { '|', ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries));
+
+            }
+        }
+    }
+
+    private static void FillData(SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> students, List<string[]> inputData)
+    {
+        foreach (string[] student in inputData)
+        {
+            if (!students.ContainsKey(student[2]))
+            {
+                students[student[2]] = new SortedDictionary<string, SortedSet<string>>();
+            }
+
+            if (!students[student[2]].ContainsKey(student[1]))
+            {
+                students[student[2]][student[1]] = new SortedSet<string>();
+            }
+
+            students[student[2]][student[1]].Add(student[0]);
+        }
+    }
+
+    private static void PrintData(SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> students)
+    {
+        foreach (var course in students)
+        {
+            Console.WriteLine(course.Key);
+
+            foreach (var lastName in course.Value)
+            {
+
+                foreach (var firstName in lastName.Value)
+                {
+                    Console.WriteLine("\t{0}, {1}", lastName.Key, firstName);
+                }
+            }
+        }
+    }
+}
---
+++ CSharp/DSA/06. Data Structures Efficiency/DataStructuresEfficiency/TradeCompany/TradeCompanyTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Wintellect.PowerCollections;
+
+public class TradeCompany
+{
+    private static Random rnd = new Random();
+    private const int numberOfArticles = 1000000;
+    private const decimal minValue = 500;
+    private const decimal maxValue = 501;
+
+    static void Main()
+    {
+
+        OrderedMultiDictionary<decimal, string> articles = new OrderedMultiDictionary<decimal, string>(true);
+        articles = LoadData(numberOfArticles);
+        PrintRange(articles, minValue, maxValue);
+    }
+
+    private static OrderedMultiDictionary<decimal, string> LoadData(int numberOfArticles)
+    {
+        OrderedMultiDictionary<decimal, string> articles = new OrderedMultiDictionary<decimal, string>(true);
+        for (int articel = 0; articel < numberOfArticles; articel++)
+        {
+            decimal randomPrice = GetRandomPrice();
+            string product = GetRandomProduct();
+            articles[randomPrice].Add(product);
+        }
+
+        return articles;
+    }
+
+    private static void PrintRange(OrderedMultiDictionary<decimal, string> articles, decimal minValue, decimal maxValue)
+    {
+        OrderedMultiDictionary<decimal, string>.View filtereList = articles.Range(minValue, true, maxValue, false);
+        foreach (var articel in filtereList)
+        {
+            Console.WriteLine("Price: {0}", articel.Key);
+
+            foreach (var name in articel.Value)
+            {
+                Console.WriteLine("\t --> {0}", name);
+            }
+        }
+    }
+
+    private static decimal GetRandomPrice()
+    {
+        decimal randomPrice = rnd.Next(numberOfArticles / 10) / 100.0m;
+        return randomPrice;
+    }
+
+    private static string GetRandomProduct()
+    {
+        string randomBarCode = GetRandomString(8, 8);
+        string randomVendor = GetRandomString(5, 10);
+        string randomTitle = GetRandomString(10, 20);
+
+        return randomBarCode + " : " + randomVendor + "/" + randomTitle;
+    }
+
+    private static string GetRandomString(int minLen, int maxLen)
+    {
+        int stringLenght = rnd.Next(minLen, maxLen);
+        char[] stringAsCharArray = new char[stringLenght];
+
+        for (int letter = 0; letter < stringLenght; letter++)
+        {
+            stringAsCharArray[letter] = (char)(rnd.Next(25) + 'a');
+        }
+
+        string randomString = new string(stringAsCharArray);
+        return randomString;
+    }
+}
