b2c9af6fc1fc47551a547f53ecf8f858fea3a8f4
wnvko <milko.venkov@gmail.com>
2015-12-28 22:46:23 +0200

    NodeJs - View Engines - ok

---
+++ SoftwareTechnoligies/EndToEndJavaScript/06. ViewEngines/Homework/MobileDevices/app.js
@@ -0,0 +1,57 @@
+﻿var http = require('http'),
+    fs = require('fs'),
+    jade = require('jade'),
+    path = require('path'),
+    common = require('./common/common.js');
+
+var PORT = 1234,
+    MAIN_PAGE_JADE_PATH = './templates/main.jade',
+    PHONES_PAGE_JADE_PATH = './templates/phones.jade',
+    TABLETS_PAGE_JADE_PATH = './templates/tablets.jade',
+    WEARABLE_PAGE_JADE_PATH = './templates/wearables.jade';
+
+http.createServer(myServerListener).listen(PORT);
+
+function myServerListener(req, res) {
+    if (req.url === '/' || req.url === '/home') {
+        parseHtml(MAIN_PAGE_JADE_PATH, common.mainModel, res, loadPage);
+    } else if (req.url === '/phones') {
+        parseHtml(PHONES_PAGE_JADE_PATH, common.phonesModel, res, loadPage);
+    } else if (req.url === '/tablets') {
+        parseHtml(TABLETS_PAGE_JADE_PATH, common.tabletsModel, res, loadPage);
+    } else if (req.url === '/wearables') {
+        parseHtml(WEARABLE_PAGE_JADE_PATH, common.wearablesModel, res, loadPage);
+    }
+}
+
+function parseHtml(templateFilePath, model, res, cb) {
+    fs.readFile(templateFilePath, 'utf-8', function (err, data) {
+        if (err) {
+            console.log(err);
+            return null;
+        }
+        
+        cb(jade.compile(data, {
+            pretty: true,
+            filename : path.join(__dirname, templateFilePath)
+        }), model, res);
+    });
+}
+
+function loadPage(result, model, res) {
+    var mainHtml = result(model);
+    if (!mainHtml) {
+        res.writeHead(404, {
+            'Content-Type' : 'text/html'
+        });
+        res.write(http.STATUS_CODES[404]);
+        res.end();
+    } else {
+        res.writeHead(200, {
+            'Content-Type' : 'text/html'
+        });
+        
+        res.write(mainHtml);
+        res.end();
+    }
+}
---
+++ SoftwareTechnoligies/EndToEndJavaScript/06. ViewEngines/Homework/MobileDevices/common/common.js
@@ -0,0 +1,42 @@
+﻿var navModel = [{
+        url: 'home',
+        title: 'Home'
+    }, {
+        url: 'phones',
+        title: 'Smart Phones'
+    }, {
+        url: 'tablets',
+        title: 'Tablets'
+    }, {
+        url: 'wearables',
+        title: 'Wearables'
+    }];
+
+var mainModel = {
+    nav: navModel,
+}
+
+var phonesModel = {
+    nav: navModel,
+    phones : ['Samsung Galaxy S70', 'Apple iPhone 56', 'Sony Xperia 34']
+};
+
+var tabletsModel = {
+    nav: navModel,
+    tablets : ['Samsung Tab S70', 'Apple iPad 56', 'Sony Xperia Z34 Tablet']
+};
+
+var wearablesModel = {
+    nav: navModel,
+    wearables : ['Samsung Gear S70', 'Apple Watch 56', 'Sony SmarthWatch 56']
+};
+
+
+module.exports = {
+    navModel : navModel,
+    mainModel: mainModel,
+    phonesModel : phonesModel,
+    tabletsModel : tabletsModel,
+    wearablesModel: wearablesModel
+}
+
