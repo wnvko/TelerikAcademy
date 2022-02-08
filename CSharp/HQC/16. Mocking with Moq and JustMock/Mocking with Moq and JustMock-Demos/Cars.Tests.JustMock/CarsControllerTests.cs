3a9bd9283fda0d161cfd5222a9328d44b357cb5f
Milko Venkov <MVenkov@infragistics.com>
2022-02-08 22:35:55 +0200

    Clean HQC folder

--- CSharp/HQC/16. Mocking with Moq and JustMock/Mocking with Moq and JustMock-Demos/Cars.Tests.JustMock/CarsControllerTests.cs
+++ CSharp/HQC/16. Mocking with Moq and JustMock/Mocking with Moq and JustMock-Demos/Cars.Tests.JustMock/CarsControllerTests.cs
@@ -13,11 +13,11 @@
     [TestClass]
     public class CarsControllerTests
     {
-        private ICarsRepository carsData; //CarRepositoryMock.carsData - all fake methods
+        private ICarsRepository carsData;
         private CarsController controller;
 
         public CarsControllerTests()
-            : this(new CarRepositoryMock())
+            : this(new JustMockCarsRepository())
         {
         }
 
@@ -36,7 +36,6 @@
         public void IndexShouldReturnAllCars()
         {
             var model = (ICollection<Car>)this.GetModel(() => this.controller.Index());
-
 
             Assert.AreEqual(4, model.Count);
         }
@@ -96,88 +95,6 @@
             Assert.AreEqual("A4", model.Model);
             Assert.AreEqual(2005, model.Year);
         }
-
-        [TestMethod]
-        public void GettingCarByIdShouldReturnADetail()
-        {
-            var model = (Car)this.GetModel(() => this.controller.Details(100));
-
-            Assert.AreEqual(1, model.Id);
-            Assert.AreEqual("Audi", model.Make);
-            Assert.AreEqual("A4", model.Model);
-            Assert.AreEqual(2005, model.Year);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(NullReferenceException))]
-        public void GettingCarByIdShouldThrowArgumentNullExceptionIfCarIsNull()
-        {
-            var nullCar = new Car();
-            nullCar = null;
-
-            var model = (Car)this.GetModel(() => this.controller.Details(nullCar.Id));
-        }
-
-        [TestMethod]
-        public void SearchCarsShouldReturnCars()
-        {
-            var searchString = "Fake String";
-            var model = (List<Car>)this.GetModel(() => this.controller.Search(searchString));
-
-            var result = new List<Car>
-            {
-                new Car { Id = 2, Make = "BMW", Model = "325i", Year = 2008 },
-                new Car { Id = 3, Make = "BMW", Model = "330d", Year = 2007 },
-            };
-
-
-            Assert.AreEqual(result.First<Car>().Id, model.First<Car>().Id);
-            Assert.AreEqual(result.First<Car>().Model, model.First<Car>().Model);
-            Assert.AreEqual(result.Last<Car>().Make, model.Last<Car>().Make);
-            Assert.AreEqual(result.Last<Car>().Year, model.Last<Car>().Year);
-        }
-
-        [TestMethod]
-        public void SortByMakeShouldReturnSortedCarsCollection()
-        {
-            var sortByString = "make";
-
-            var model = (List<Car>)this.GetModel(() => this.controller.Sort(sortByString));
-
-            var result = new CarRepositoryMock().FakeCarCollection.OrderBy(m => m.Make).ToList();
-
-            Assert.AreEqual(result.First<Car>().Id, model.First<Car>().Id);
-            Assert.AreEqual(result.First<Car>().Model, model.First<Car>().Model);
-            Assert.AreEqual(result.Last<Car>().Make, model.Last<Car>().Make);
-            Assert.AreEqual(result.Last<Car>().Year, model.Last<Car>().Year);
-        }
-
-
-        [TestMethod]
-        public void SortByYearShouldReturnSortedCarsCollection()
-        {
-            var sortByString = "year";
-
-            var model = (List<Car>)this.GetModel(() => this.controller.Sort(sortByString));
-
-            var result = new CarRepositoryMock().FakeCarCollection.OrderBy(m => m.Make).ToList();
-
-            Assert.AreEqual(result.First<Car>().Id, model.First<Car>().Id);
-            Assert.AreEqual(result.First<Car>().Model, model.First<Car>().Model);
-            Assert.AreEqual(result.Last<Car>().Make, model.Last<Car>().Make);
-            Assert.AreEqual(result.Last<Car>().Year, model.Last<Car>().Year);
-        }
-
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void SortByWrongParameterShouldThrowArgumentException()
-        {
-            var sortByString = "Wrong parameter";
-
-            var model = (List<Car>)this.GetModel(() => this.controller.Sort(sortByString));
-        }
-
 
         private object GetModel(Func<IView> funcView)
         {
